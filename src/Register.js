17991f41360ae537c8a76d57ada527bc073333c1
Hulknahid <hulknahid8@gmail.com>
2022-03-22 21:25:57 +0600

    Adding form with functional and class component

--- src/Register.js
+++ src/Register.js
@@ -1,33 +1,45 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Form,FormGroup,Label,Input,Container,Button} from 'reactstrap'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+const App =()=> {
+    // firstname: '',
+    //     lastname: '',
+    //     email: '',
+    //     password: '',
+    //     mobile: '',
+    //     url: '',
+    //     errors:{}
+
+
+
+    const [user,setUser] = useState({
             firstname: '',
             lastname: '',
             email: '',
             password: '',
             mobile: '',
             url: '',
-            errors:{}
+
+    })
+
+    const [users,setUsers] = useState([])
+    useEffect(()=>{
+        let userList = localStorage.getItem('@USER');
+        console.log(userList)
+        if(userList != null){
+            setUsers([...JSON.parse(userList)])
         }
-
-    }
-
+    },[])
     //set state on change value
-    changeHandler=(e)=>{
+    const changeHandler=(e)=>{
         let {name,value} = e.target
-        this.setState({
-            [name]:value
-        })
+        setUser({...user,[name]:value})
     }
 
     //submitting the form
-    submitForm=()=>{
-        let {firstname,lastname,email,password,mobile,url} = this.state;
+    const submitForm=()=>{
+        let {firstname,lastname,email,password,mobile,url} = user;
         let userJSON = {
             firstname,
             lastname,
@@ -36,7 +48,19 @@ class App extends Component {
             mobile,
             url
         }
-        localStorage.setItem('@USER',JSON.stringify(userJSON))
+        //setting data in localstorage
+        //to store data must need to make the
+        //object to json using json.stringify
+
+        let list = users
+
+        list.push(userJSON)
+
+
+        localStorage.setItem('@USER',JSON.stringify(list))
+
+        setUsers([...list])
+
         toast('User Added Successfully!', {
             position: "top-right",
             autoClose: 5000,
@@ -46,22 +70,22 @@ class App extends Component {
             draggable: true,
             progress: undefined,
         });
-        this.clearState()
+        clearState()
     }
-    clearState=()=>{
-        this.setState({
+    const clearState=()=>{
+        setUser({
             firstname: '',
             lastname: '',
             email: '',
             password: '',
             mobile: '',
             url: '',
-            errors:{}
         })
     }
-    render() {
 
-        let {firstname,lastname,email,password,mobile,url} = this.state;
+        let {firstname,lastname,email,password,mobile,url} = user;
+
+
 
         return (
             <Container>
@@ -74,7 +98,7 @@ class App extends Component {
                         </Label>
                         <Input
                             value={firstname}
-                            onChange={this.changeHandler}
+                            onChange={changeHandler}
                             id="firstname"
                             name="firstname"
                             placeholder="First Name"
@@ -87,7 +111,7 @@ class App extends Component {
                         </Label>
                         <Input
                             value={lastname}
-                            onChange={this.changeHandler}
+                            onChange={changeHandler}
                             id="lastname"
                             name="lastname"
                             placeholder="Last Name"
@@ -100,7 +124,7 @@ class App extends Component {
                         </Label>
                         <Input
                             value={email}
-                            onChange={this.changeHandler}
+                            onChange={changeHandler}
                             id="exampleEmail"
                             name="email"
                             placeholder="with a placeholder"
@@ -113,7 +137,7 @@ class App extends Component {
                         </Label>
                         <Input
                             value={password}
-                            onChange={this.changeHandler}
+                            onChange={changeHandler}
                             id="examplePassword"
                             name="password"
                             placeholder="password placeholder"
@@ -126,7 +150,7 @@ class App extends Component {
                         </Label>
                         <Input
                             value={mobile}
-                            onChange={this.changeHandler}
+                            onChange={changeHandler}
                             id="mobile"
                             name="mobile"
                             placeholder="Mobile"
@@ -139,7 +163,7 @@ class App extends Component {
                         </Label>
                         <Input
                             value={url}
-                            onChange={this.changeHandler}
+                            onChange={changeHandler}
                             id="exampleUrl"
                             name="url"
                             placeholder="url placeholder"
@@ -148,11 +172,45 @@ class App extends Component {
                     </FormGroup>
 
                     <FormGroup>
-                        <Button onClick={this.submitForm} className="btn btn-primary btn-lg">Save User</Button>
+                        <Button onClick={submitForm} className="btn btn-primary btn-lg">Save User</Button>
 
                     </FormGroup>
+
+
+                    <table className="table table-striped table-bordered">
+                        <thead>
+                        <tr>
+                            <td>First Name</td>
+                            <td>Last name</td>
+                            <td>Email</td>
+                            <td>Password</td>
+                            <td>Mobile</td>
+                            <td>Url</td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {users.length > 0 && users.map((user,key)=>{
+                            return (
+                                <tr>
+                                    <td>{user.firstname}</td>
+                                    <td>{user.lastname}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.password}</td>
+                                    <td>{user.mobile}</td>
+                                    <td>{user.url}</td>
+                                </tr>
+                            )
+                        })}
+                        </tbody>
+
+
+
+
+                    </table>
+
+
             </Container>
         );
-    }
+
 }
 export default App;
